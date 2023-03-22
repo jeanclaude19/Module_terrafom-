d160f90e4907dd8b838952f9b6258c7443374ea3
cmonthe2 <88361315+cmonthe2@users.noreply.github.com>
2023-03-21 21:44:20 -0400

    Update s3_module.tf

--- s3_module.tf
+++ s3_module.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "cameron30-s3-bucket"
+  bucket = "cameron30-s3-bucket-uutttgggjjgjg"
   acl    = "private"
 
   versioning = {
